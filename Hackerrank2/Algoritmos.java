c568a7174603de498ec6cc7b9f45ebd6f23212a4
MarianoNavarrete <mariano@mariano-GS63VR-7RF>
2020-11-24 14:22:22 -0500

    Ejercicios de hackerrrank

---
+++ Hackerrank2/Algoritmos.java
@@ -0,0 +1,12 @@
+
+public class Algoritmos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Saludar();
+	}
+	public static void Saludar() {
+		System.out.println("Hola");
+	}
+
+}
